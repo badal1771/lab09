faa8f9fd1888dd50417916139ebf59bb15be1cf4
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-11-10 00:03:37 +0530

    Add files via upload

---
+++ lab09_17.cpp
@@ -0,0 +1,15 @@
+//include library
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+//create main
+int main()
+{char str1[]="my name" ,str2[]="badal singh";
+char src[30];
+cout<<strcat(str1,str2)<<endl;
+cout<<strcpy(src,str2)<<endl;
+return 0;
+}
+
